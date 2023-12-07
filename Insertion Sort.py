b751a7a7099c9109e0a082e4f37c0a28b6755e19
krystique <147011154+krystique@users.noreply.github.com>
2023-12-07 12:19:44 +0800

    Update Insertion Sort.py

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -1,1 +1,19 @@
 print("<<< INSERTion SORT >>>\n")
+
+numbers = []
+for index in range(5):
+    num = int(input("Enter a number: "))
+    result = numbers.append(num)
+    result = ' '.join(str(item) for item in numbers)
+
+print(f"\n{result}")
+
+while True:
+    
+
+    order = input('''\nHow do you like to sort your numbers?
+
+Ascending  [A]
+Descending [D]
+
+A or D: ''')
